897d493db134a6ece0013ce1a77282a3c939c1f0
venkyvirat123 <venkataramanapodugu087@gmail.com>
2024-12-28 19:35:20 +0530

    added files

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,14 @@
 def call(String dockerHubUsername, String imageName) {
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=5f428cbba5msh53a1a25add324f7p1a7b6ejsnffcd9bc28d0c -t ${imageName} ."
+    // Ensure BuildKit is used
+    env.DOCKER_BUILDKIT = '1'
+
+    // Specify Dockerfile path explicitly if not in the root
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=5f428cbba5msh53a1a25add324f7p1a7b6ejsnffcd9bc28d0c -t ${imageName} -f ./Dockerfile ."
     
+    // Tag the image with the username and latest tag
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
    
+    // Push the image to Docker Hub
     withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
         sh "docker push ${dockerHubUsername}/${imageName}:latest"
     }
